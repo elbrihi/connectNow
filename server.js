e5467723b09f67d908749b760d89a8eba24cbeae
elbrihi <elbrihiyasine3@gmail.com>
2022-06-02 15:20:27 +0200

    ""

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/connectNow'));
+app.use(express.static(__dirname + '/dist/angular-mat'));
 app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/connectNow/index.html'));});
+{res.sendFile(path.join(__dirname+'/dist/angular-mat/index.html'));});
 
   app.listen(process.env.PORT || 8080);
